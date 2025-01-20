f1030ae858d5c8a99407e2aa7691ada31dc0e38a
Preethi <pattlollapreethi@gmail.com>
2025-01-20 23:16:26 +0530

    commit-14

--- routes/User.js
+++ routes/User.js
@@ -2,24 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 // Define routes for users
-router.get('/', (req, res) => {
-    res.send('Get all users');
-});
+const {login,signup,sendotp,changePassword,}=require("../controllers/Auth")
+const {resetPasswordToken,resetPassword}=require("../controllers/ResetPassword")
 
-router.get('/:id', (req, res) => {
-    res.send(`Get user with ID: ${req.params.id}`);
-});
-
-router.post('/', (req, res) => {
-    res.send('Register a new user');
-});
-
-router.put('/:id', (req, res) => {
-    res.send(`Update user with ID: ${req.params.id}`);
-});
-
-router.delete('/:id', (req, res) => {
-    res.send(`Delete user with ID: ${req.params.id}`);
-});
+const {auth}=require("../middlewares/auth")
+router.post("/login",login)
+router.post("/signup",signup)
+router.post("/sendotp",sendotp)
+router.post("/changepassword",changePassword)
+router.post("/reset-password-token",resetPasswordToken)
+router.post("/reset-password",resetPassword)
 
 module.exports = router;
